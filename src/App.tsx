248f4f75c628292f2d868587e1e287bc4a7b5f9e
Miguel Nicolas <miguelqnicolas@gmail.com>
2021-01-01 10:41:27 -0500

    Sync progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import QuestionCard from './components/QuestionCard';
+
+const API_URL = 'https://opentdb.com/api.php?amount=10&category=22&difficulty=easy&type=multiple';
 
 const App = () => {
   return (
     <>
+      <h1>Geography Quiz</h1>
+      <p>Test your knowledge of geography!</p>
+      <button>Start</button>
     </>
   );
 };
---
+++ src/components/QuestionCard.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const QuestionCard: React.FC = () => {
+    return(
+        <>
+        </>
+    );
+};
+
+export default QuestionCard;
